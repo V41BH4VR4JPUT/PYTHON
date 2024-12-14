04bd5565b012380ab1ac39926df5d15ccbc3ab20
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2024-12-14 10:11:03 +0530

    first program and comments usage

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hello World")
+import pandas #only for example showcasing
+import hashlib #only for example showcasing
+# practie code of first program in python and comments
+print ("Hello World \n"  "Hi \n"  "Vaibhav Kumar")
