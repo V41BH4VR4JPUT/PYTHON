6c79e0bfc2dbee06d3a36ecea0a09bd09cf4f856
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2024-12-14 10:37:19 +0530

    escape sequence characters

---
+++ py2.py
@@ -0,0 +1,9 @@
+"""
+using of escape sequence characters in python
+print(object(s), sep=separator, end=end, file=file, flush=flush)
+print("This doesnt "execute")
+print("This will \" execute")
+"""
+print("vaibhav\n" , 28 , 30 , sep=":")
+print("hello world   \" Myself ricky \" ")
+print(1, 3, 5, 6, 7, 8, end="\nAll numbers")
