f824899cadafa81589490349996fbd1b3d9d84da
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2024-12-17 20:59:28 +0530

    variables and data types

---
+++ py3.py
@@ -0,0 +1,21 @@
+b = "vaibhav"
+vaibhav = 28
+print(vaibhav)
+
+a = 30
+print(a)
+c = 28
+print(a + c)
+print(type(a),type(b),type(c))
+
+d = complex(28,30)
+print(d,type(d))
+
+list = [1 , 2.4 , "vaibhav" , ["me" , 21 ]]
+print(list)
+
+tuple = (1 , 2.4 , "vaibhav" , ["me" , 21 ])
+print(tuple)
+
+dict = {"name" : "vaishnavi" , "age" : 22, "status" : "mine" }
+print(dict)
