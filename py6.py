d6b727fbe7b9efbebcce03146b24ff60991c13dd
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2024-12-17 21:45:43 +0530

    Taking Input From User

---
+++ py6.py
@@ -0,0 +1,10 @@
+#Taking Input from user
+
+name = input("Enter your name : ")
+print("Your name is : " , name)
+
+age = input("Enter your age : ")
+print("Your age is : " , age)
+
+print("Your name is : " , name , "and your age is : " , age)
+
