1591c6243a126cf9d6f7c43575e68089f4122eb1
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2024-12-17 21:58:06 +0530

    Strings

---
+++ py7.py
@@ -0,0 +1,9 @@
+# Strings in Python
+
+name = "vaibhav"
+code = """ mvpwfpanwv;awpgvh3pvnpvnmp[HG[NV;KL  JWE[ J[NMG J [F JGJw gjw[gjw'VMJG[wjg'wv'ajg]]]]]]] """
+print("My name is : " , name)
+print("for loop")
+
+for characters in code:
+    print(characters)
