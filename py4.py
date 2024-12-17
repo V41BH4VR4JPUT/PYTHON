5a78e930cea1993754aee8832d91864e10b08a81
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2024-12-17 21:13:19 +0530

    EXERCISE 1

---
+++ py4.py
@@ -0,0 +1,13 @@
+# Exercise : Creating a Calculator performing simple operations
+
+a = 28
+b = 30
+print("TWO NUMBERS ARE and b : " , a , "and" , b)
+print("*************performing simple operations*************")
+print("Addition : " , a + b)
+print("Subtraction : " , a - b)
+print("Multiplication : " , a * b)
+print("Division : " , a / b)
+print("Floor division : " , a // b)
+print("Modulus : " , a % b)
+print("Exponential : " , a ** b)
