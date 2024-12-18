9db6bff62e826eaa897c1f2cd818641dcd3edfe3
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2024-12-18 12:40:32 +0530

    string slicing

--- py8.py
+++ py8.py
@@ -1,6 +1,13 @@
 # string Slicing
 
 name = "vaibhav"
-print(name[0:3])
+print(name[0:3]) # string slicing example
 print(name[0:6:2])
-print(name[::-1])
+print(name[:-1])
+# string length
+print(len(name))
+len1 = len(name)
+print(len1)
+print(name[-3:-1])
+nm = "harry"
+print(nm[-4:-2])
