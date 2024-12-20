aa367ab1b3d6a86c0c44281e1522f0eefad5901b
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2024-12-20 15:47:19 +0530

    Source Code

---
+++ Python_codes/py13.py
@@ -0,0 +1,16 @@
+# while loop in python
+
+#example 1
+count = 5
+while (count > 0):
+  print(count )
+  count = count - 1
+
+#example 2
+print("\n")   
+x = 5
+while (x > 0):
+    print(x)
+    x = x - 1
+else:
+    print('Loop is over')
