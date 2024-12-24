e3b3e914f03afd8f5436af7f7159f7538f08a155
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2024-12-24 08:06:57 +0530

    Source Code

---
+++ Python_codes/py18.py
@@ -0,0 +1,84 @@
+# List Methods
+
+# Sorting the List
+# ascending order
+colors = ["voilet", "indigo", "blue", "green"]
+colors.sort()
+print(colors)
+
+num = [4,2,5,3,6,1,2,1,2,8,9,7]
+num.sort()
+print(num)
+print("\n")
+# DESCENDING ORDER
+colors = ["voilet", "indigo", "blue", "green"]
+colors.sort(reverse=True)
+print(colors)
+
+num = [4,2,5,3,6,1,2,1,2,8,9,7]
+num.sort(reverse=True)
+print(num)
+print("\n")
+
+# Reversing the List
+colors = ["voilet", "indigo", "blue", "green"]
+colors.reverse()
+print(colors)
+
+num = [4,2,5,3,6,1,2,1,2,8,9,7] 
+num.reverse()
+print(num) 
+print("\n")
+
+# index() method
+colors = ["voilet", "green", "indigo", "blue", "green"]
+print(colors.index("green"))
+
+num = [4,2,5,3,6,1,2,1,3,2,8,9,7]
+print(num.index(3))
+print("\n")
+
+# count() method
+colors = ["voilet", "green", "indigo", "blue", "green"]
+print(colors.count("green"))
+
+num = [4,2,5,3,6,1,2,1,3,2,8,9,7]
+print("\n")
+
+# copy() method
+
+colors = ["voilet", "green", "indigo", "blue"]
+newlist = colors.copy()
+print(colors)
+print(newlist)
+print("\n")
+
+# append() method
+colors = ["voilet", "indigo", "blue"]
+colors.append("green")
+print(colors)
+print("\n")
+
+# insert() method
+colors = ["voilet", "indigo", "blue"]
+#           [0]        [1]      [2]
+
+colors.insert(1, "green")   #inserts item at index 1
+# updated list: colors = ["voilet", "green", "indigo", "blue"]
+#       indexs              [0]       [1]       [2]      [3]
+
+print(colors)
+print("\n")
+
+# extend() method
+#add a list to a list
+colors = ["voilet", "indigo", "blue"]
+rainbow = ["green", "yellow", "orange", "red"]
+colors.extend(rainbow)
+print(colors)
+print("\n")
+
+# Concatenating two lists
+colors = ["voilet", "indigo", "blue", "green"]
+colors2 = ["yellow", "orange", "red"]
+print(colors + colors2)
