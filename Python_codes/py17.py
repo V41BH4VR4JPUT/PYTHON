25e673a01d740d3df8cb9d3b4da87255b18d2f4e
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2024-12-24 07:47:34 +0530

    Source Code

---
+++ Python_codes/py17.py
@@ -0,0 +1,86 @@
+# Lists in Python
+#exapmle 1
+lst1 = [1,2,2,3,5,4,6]
+lst2 = ["Red", "Green", "Blue"]
+print(lst1)
+print(lst2)
+print("\n")
+
+#example 2
+details = ["Abhijeet", 18, "FYBScIT", 9.8]
+print(details,"\n")
+
+# list indexes
+colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+#          [0]      [1]     [2]      [3]      [4]
+
+#Accessing the list items
+#positive indexing
+colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+#          [0]      [1]     [2]      [3]      [4]
+print(colors[2])
+print(colors[4])
+print(colors[0])
+print("\n")
+
+#negative indexing
+colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+#          [-5]    [-4]    [-3]     [-2]      [-1]
+print(colors[-1])
+print(colors[-3])
+print(colors[-5])
+print("\n")
+
+#checking whether item is present in the list or not
+colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+if "Yellow" in colors:
+    print("Yellow is present.")
+else:
+    print("Yellow is absent.")
+
+print("\n")
+
+colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+if "Orange" in colors:
+    print("Orange is present.")
+else:
+    print("Orange is absent.")
+
+print("\n")
+
+#RANGE OF INDEXES
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[3:7])	#using positive indexes
+print(animals[-7:-2])	#using negative indexes'
+print("\n")
+
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[4:])	#using positive indexes
+print(animals[-4:])	#using negative indexes
+print("\n")
+
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[:6])	#using positive indexes
+print(animals[:-3])	#using negative indexes
+print("\n")
+
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[::2])		#using positive indexes
+print(animals[-8:-1:2])	#using negative indexes
+print("\n")
+
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[1:8:3])
+print("\n")
+
+#List Comprehension
+#example 1
+names = ["Milo", "Sarah", "Bruno", "Anastasia", "Rosa"]
+namesWith_O = [item for item in names if "o" in item]
+print(namesWith_O)
+print("\n")
+
+#example 2
+names = ["Milo", "Sarah", "Bruno", "Anastasia", "Rosa"]
+namesWith_O = [item for item in names if (len(item) > 4)]
+print(namesWith_O)
