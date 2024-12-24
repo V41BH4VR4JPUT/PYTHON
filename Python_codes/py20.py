aa1b78f6f79202920100073f8699b5f502174926
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2024-12-24 08:37:22 +0530

    Source Code

---
+++ Python_codes/py20.py
@@ -0,0 +1,35 @@
+#Operations on Tuples
+
+#Tuples are immutable, hence if you want to add, remove or change tuple items, then first you must convert the tuple to a list. Then perform operation on that list and convert it back to tuple.
+
+#Converting tuple to list
+
+countries = ("Spain", "Italy", "India", "England", "Germany")
+temp = list(countries)
+temp.append("Russia")       #add item 
+temp.pop(3)                 #remove item
+temp[2] = "Finland"         #change item
+countries = tuple(temp)
+print(countries)
+print("\n")
+
+#tuple Concatenation
+countries = ("Pakistan", "Afghanistan", "Bangladesh", "ShriLanka")
+countries2 = ("Vietnam", "India", "China")
+southEastAsia = countries + countries2
+print(southEastAsia)
+print("\n")
+
+#Tuple Methods
+#count() method
+
+Tuple1 = (0, 1, 2, 3, 2, 3, 1, 3, 2)
+res = Tuple1.count(3)
+print('Count of 3 in Tuple1 is:', res)
+print("\n")
+
+#index() method
+Tuple = (0, 1, 2, 3, 2, 3, 1, 3, 2)
+res = Tuple.index(3)
+print('First occurrence of 3 is', res)
+
