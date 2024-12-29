2402ff44117ec07a0f84333ff473aa001c1b6492
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2024-12-29 16:11:40 +0530

    Source Code

---
+++ Python_codes/py28.py
@@ -0,0 +1,22 @@
+# Dictionary Methods
+info = {'name':'Vaibhav', 'age':19, 'eligible':True}
+print(info)
+#Update()   
+info.update({'age':22})
+info.update({'DOB':2001})
+print(info)
+ing = {'name':'Vaibhav', 'age':19, 'eligible':True}
+print(ing)
+#clear()
+ing.clear()
+print(ing)
+#pop()
+info.pop('DOB')
+print(info)
+#popitem()
+info.popitem()
+print(info)
+#del
+del info['age']
+print(info)
+del ing
