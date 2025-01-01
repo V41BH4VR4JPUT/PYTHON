3d62e8f40b7b51457e95bf70da626607fa284ec3
Vaibhav Kumar <141570549+V41BH4VR4JPUT@users.noreply.github.com>
2025-01-01 14:00:18 +0530

    Strings in Python

--- Python_codes/py7.py
+++ Python_codes/py7.py
@@ -4,6 +4,5 @@ name = "vaibhav"
 code = """ mvpwfpanwv;awpgvh3pvnpvnmp[HG[NV;KL  JWE[ J[NMG J [F JGJw gjw[gjw'VMJG[wjg'wv'ajg]]]]]]] """
 print("My name is : " , name)
 print("for loop")
-
 for characters in code:
     print(characters)
