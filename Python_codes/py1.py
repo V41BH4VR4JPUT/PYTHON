4b622a3cc3e6d1de78ee42373998e9155dccb6fb
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2025-01-01 13:52:00 +0530

    Printing Statement and Importing Modules

--- Python_codes/py1.py
+++ Python_codes/py1.py
@@ -2,3 +2,12 @@ import pandas #only for example showcasing
 import hashlib #only for example showcasing
 # practie code of first program in python and comments
 print ("Hello World \n"  "Hi \n"  "Vaibhav Kumar")
+'''
+to import modules in python we use import keyword
+import pandas
+import hashlib
+and the procedure to import modules from cmd is
+pip install pandas
+pip install hashlib
+
+'''
