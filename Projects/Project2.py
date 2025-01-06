abbb66692c00c4ec0738e4cb3c6a210346ac8502
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2025-01-06 14:29:30 +0530

    Task Tracker and Reminder System

---
+++ Projects/Project2.py
@@ -0,0 +1,75 @@
+#Task Tracker and Reminder System
+
+def add_task(task_list):
+    task = input("\nEnter the task: ").strip()
+    task_list.append({"task": task, "Status of Task": "Pending"})
+    print(f"Task '{task}' added successfully.")
+    print("\n")
+
+def view_tasks(task_list):
+    if not task_list:
+        print("No tasks found.")
+    else:
+        print("\nTasks:")
+        for i in range(len(task_list)):
+            task = task_list[i]
+            print(f"{i + 1}. {task['task']} - {task['Status of Task']}")
+        print("\n")    
+
+def mark_as_completed(task_list):
+    view_tasks(task_list)
+    if task_list:
+        try :
+            task_index = int(input("Enter the task number to mark as completed: ")) - 1
+            if 0 <= task_index < len(task_list):
+                task_list[task_index]["Status of Task"] = "Completed"
+                print("Task marked as completed.")
+            else:
+                print("Invalid task number.")
+        except ValueError:
+            print("Invalid input. Please enter a valid task number.")
+
+def delete_task(task_list):
+    view_tasks(task_list)
+    if task_list:
+        try :
+            task_index = int(input("Enter the task number to delete: ")) - 1
+            if 0 <= task_index < len(task_list):
+                deleted_task = task_list.pop(task_index)
+                print(f"Task '{deleted_task['task']}' deleted.")
+            else:
+                print("Invalid task number.")
+        except ValueError:
+            print("Invalid input. Please enter a valid task number.")
+
+            
+def mainMenu():
+    print("Welcome to Task Tracker and Reminder System".center(80, "-"))
+    print("Task Tracker Menu:")
+
+    task_list = []
+    while True:
+        print("1. Add Task")
+        print("2. View Tasks")
+        print("3. Mark Task as Completed")
+        print("4. Delete Task")
+        print("5. Exit")
+
+        choice = input("Enter your choice: ").strip()
+        match choice:
+            case "1":
+                add_task(task_list)
+            case "2":
+                view_tasks(task_list)
+            case "3":
+                mark_as_completed(task_list)
+            case "4":
+                delete_task(task_list)
+            case "5":
+                if choice == "5":
+                    print("Goodbye! Have a nice day.")
+                    break           
+            case _:
+                print("Invalid choice. Please try again.")
+
+mainMenu()
