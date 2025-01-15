305f0ad5947d33e5676650582abcdec8299b411c
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2025-01-15 19:31:33 +0530

    Lambda Function

---
+++ Python_codes/py37.py
@@ -0,0 +1,8 @@
+# lambda function in python
+# def double(x):
+#     return x * 2
+# print (double(5))
+
+#using Lambda
+print((lambda x: x * 2)(5))
+(lambda x , y : print(f"{x} + {y} = {x + y}"))(5, 10)
