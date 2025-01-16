d4252348518f8170a367e84c327c0af0953741f1
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2025-01-16 14:31:00 +0530

    Classes and Object in Python

---
+++ Python_codes/py40.py
@@ -0,0 +1,16 @@
+# class and objects in python
+
+class details :
+    name = "vaibhav"
+    age = 22
+    occupation = "student"
+    def info(self):
+        print("Name : " , self.name , "\nAge : " , self.age , "\nOccupation : " , self.occupation)
+
+obj = details()
+obj.name = "Vaishnavi"
+obj.age = 23
+obj.occupation = "student"
+obj.info()
+obj2 = details()
+obj2.info()
