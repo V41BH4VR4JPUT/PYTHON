6b954e2e915c01bca371cc037b701f6c00f93bad
Vaibhav Kumar <141570549+V41BH4VR4JPUT@users.noreply.github.com>
2025-01-20 23:04:42 +0530

    Finally keyword

--- Python_codes/py31.py
+++ Python_codes/py31.py
@@ -1,7 +1,6 @@
 # Finally Keyword in Python
 '''
 The finally code block is also a part of exception handling. When we handle exception using the try and except block, we can include a finally block at the end. The finally block is always executed, so it is generally used for doing the concluding tasks like closing file resources or closing database connection or may be ending the program execution with a delightful message.
-
 Syntax:
 try:
    #statements which could generate 
@@ -11,7 +10,6 @@ except:
 finally:
     #block of code which is going to 
     #execute in any situation
-
 '''
 def func1():
   try:
