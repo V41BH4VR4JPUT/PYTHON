eb7cb142c3a8ae8821e7fbbfc9753d36afe74fbf
Vaibhav Kumar <141570549+V41BH4VR4JPUT@users.noreply.github.com>
2025-01-25 12:08:47 +0530

    Class Variable vs Instance Variable

--- Python_codes/py48.py
+++ Python_codes/py48.py
@@ -1,16 +1,16 @@
 # class variable and instance variable
 """  Class Variable """
-# class m():
-#     c_m = 0
-#     def __init__(self):
-#         m.c_m += 1
-#     def print_m(self):
-#         print(m.c_m)
+class m():
+    c_m = 0
+    def __init__(self):
+        m.c_m += 1
+    def print_m(self):
+        print(m.c_m)
 
-# o1 = m()
-# o1.print_m()
-# o2 = m()
-# o2.print_m()
+o1 = m()
+o1.print_m()
+o2 = m()
+o2.print_m()
 
 """  Instance Variable """
 class i():
