3f4e2c510adee1f57ff74e9aee66ee78b5ad24fe
Vaibhav Kumar <141570549+V41BH4VR4JPUT@users.noreply.github.com>
2025-01-31 13:35:03 +0530

    For Loop

--- Python_codes/py12.py
+++ Python_codes/py12.py
@@ -1,12 +1,11 @@
 # for loop in python
 #example 1
-
 name = " vaibhav "
 for i in name:
     print(i, end="-")
-print("\n")
-#example 2
+print("\n")\
 
+#example 2
 CarBRands = ["BMW", "Audi", "Mercedes", "Toyota", "Ford"]
 for x in CarBRands:
     print(x)
@@ -16,6 +15,5 @@ for x in CarBRands:
 
 #example 3
 # for loop with range function
-
 for v in range(0, 30 , 5):
     print(v , end="<")
