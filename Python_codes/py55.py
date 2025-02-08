d91ac502e1e45d9cc28749ca675e1f01646960e9
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2025-02-08 17:41:11 +0530

    Multiple Inheritance

--- Python_codes/py55.py
+++ Python_codes/py55.py
@@ -1,1 +1,25 @@
 # Multiple Inheritance in Python
+
+class parent1 : 
+    def __init__(self , name , id):
+        self.name = name
+        self.id = id
+    def display(self):
+        print(f"Name: {self.name} and ID: {self.id}")
+
+class parent2 :
+    def __init__(self , age):
+        self.age = age
+    def display(self):
+        print(f"Age: {self.age}")
+
+class child(parent1 , parent2):
+    def __init__(self , name , id , age):
+        parent1.__init__(self , name , id)
+        parent2.__init__(self , age)
+    def display(self):
+        parent1.display(self)
+        parent2.display(self)
+    
+c = child("John" , 101 , 25)
+c.display()
