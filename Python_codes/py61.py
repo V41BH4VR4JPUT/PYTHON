45fa3a74b431283d3195631664ffcb1b81ef01ba
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2025-02-09 17:32:17 +0530

    Creating Command Line Utilities

---
+++ Python_codes/py61.py
@@ -0,0 +1,27 @@
+# Creating command line utilities with argparse in Python
+
+import argparse
+
+def download_file(url , local_filename):
+    local_filename = url.split('/')[-1]
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                #if chunk: 
+                f.write(chunk)
+    return local_filename
+parser = argparse.ArgumentParser()
+
+parser.add_argument("url" , help = "Enter the URL of the website")
+parser.add_argument("output" , help = "Enter the name of the output file")
+
+args = parser.parse_args()
+
+print(args.url)
+print(args.output)
+
+download_file(args.url , args.output)
