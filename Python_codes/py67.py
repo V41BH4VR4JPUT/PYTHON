cdd02ff35d1ae31288175fc5cf94cdeaab545930
V41BH4VR4JPUT <2812vaibhav@gmail.in>
2025-02-12 18:14:00 +0530

    Multithreading

---
+++ Python_codes/py67.py
@@ -0,0 +1,28 @@
+import threading 
+def my_func():
+    print("hello form thread", threading.current_thread().name)
+
+t = threading.Thread(target=my_func)
+t.start()
+t.join()
+
+def increment(counter, lock):
+    for i in range(10000):
+        lock.acquire()
+        counter += 1
+        lock.release()
+
+if __name__ == '__main__':
+    counter = 0
+    lock = threading.Lock()
+
+    threads = []
+    for i in range(2):
+        thread = threading.Thread(target=increment, args=(counter, lock))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    print("Counter value:", counter)
