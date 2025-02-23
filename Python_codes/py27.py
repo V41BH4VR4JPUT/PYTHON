6042f604fbaeca28c3758236f80f562b14d073eb
Vaibhav Kumar <141570549+V41BH4VR4JPUT@users.noreply.github.com>
2025-02-23 22:51:40 +0530

    Dictionary

--- Python_codes/py27.py
+++ Python_codes/py27.py
@@ -1,6 +1,5 @@
 # Dictionaries in Python
 # Dictionaries are ordered collection of data items. They store multiple items in a single variable. Dictionary items are key-value pairs that are separated by commas and enclosed within curly brackets {}.
-
 # Example:
 info = {'First name' : 'Vaibhav' , 'Last name' : 'Kumar', 'Age' : 21, 'City' : 'Delhi'}
 print(info)
